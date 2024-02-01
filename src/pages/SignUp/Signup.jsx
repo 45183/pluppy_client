174a746e8fa67648bf8df01434963beaca43cde2
junoKR <147118535+junoKR@users.noreply.github.com>
2024-02-01 15:11:48 +0900

    Add files via upload

--- src/pages/SignUp/Signup.jsx
+++ src/pages/SignUp/Signup.jsx
@@ -3,24 +3,29 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
-  const User = {
-    email: 'test@example.com',
-    pw: 'test2323@@@',
-  };
+
 
   const [email, setEmail] = useState('');
   const [pw, setPw] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const [checkbox, setCheckbox] = useState('');
 
   const [pwc, setPwc] = useState('');
 
   const [emailValid, setEmailValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
   const [pwcValid, setPwcValid] = useState(false);
+  const [nameValid, setNameValid] = useState(false);
+  const [phoneValid, setPhoneValid] = useState(false);
+  const [checkValid, setCheckValid] = useState(false);
+
 
   const [notAllow, setNotAllow] = useState(true);
+  
   const [terms, setTerms] = useState(false);
+
+  const [checked, setChecked] = useState([false, false, false, false]);
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -47,40 +52,77 @@ function SignUp() {
     setPwcValid(passwordMatch && passwordValid);
   };
 
+
+
   const handleName = (e) => {
     setName(e.target.value)
+    const regex=
+    /^[가-힣]{2,}$/;
+    setNameValid(regex.test(e.target.value))
   }
   const handlePhone = (e) => {
     setPhone(e.target.value)
+    const regex=
+    /^(\d{2,3})(\d{3,4})(\d{4})$/;
+    setPhoneValid(regex.test(e.target.value));
   }
 
+  const handleCheckbox= (e)=>{
+    setCheckbox(e.target.checked)
+    setCheckValid(e.target.checked)
+  }
 
   const onClickTermButton = (e) => {
     setTerms(!terms);
   };
 
 
+  const handleChange = (index, e) => {
+    const updatedChecked = [...checked];
+    updatedChecked[index] = e.target.checked;
+    setChecked(updatedChecked);
+
+    const allChecked = updatedChecked.every((item) => item);
+    setNotAllow(!(emailValid && pwValid && pwcValid && nameValid && phoneValid && allChecked));
+  };
+
+
+  const ParentChecked = checked[0] && checked[1] && checked[2] && checked[3];
+
+  const children = (
+    <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
+      <label>
+        <input value={checkbox} type="checkbox" checked={checked[0]} onChange={
+          (e) => handleChange(0, e)} />
+        [필수] 만 14세 이상입니다.
+      </label>
+      <label>
+        <input value={checkbox}  type="checkbox" checked={checked[1]} onChange={
+          (e) => handleChange(1, e)} />
+        [필수] 이용약관 동의
+      </label>
+      <label>
+        <input value={checkbox}  type="checkbox" checked={checked[2]} onChange={
+          (e) => handleChange(2, e)} />
+        [필수] 개인정보 수집 및 이용 동의
+      </label>
+      <label>
+        <input value={checkbox}  type="checkbox" checked={checked[3]} onChange={
+          (e) => handleChange(3, e)} />
+        [필수] 개인정보 제 3자 제공 동의
+      </label>
+    </div>
+  );
+
   useEffect(() => {
-    if (emailValid && pwValid) {
+    if (emailValid && pwValid && pwcValid && nameValid && phoneValid && checkValid) {
       setNotAllow(false);
     } else {
       setNotAllow(true);
     }
-  }, [emailValid, pwValid]);
+  }, [emailValid, pwValid, pwcValid, nameValid, phoneValid, checkValid]);
 
-  const [checked, setChecked] = useState([false, false]);
 
-  const handleChange1 = (e) => {
-    setChecked([e.target.checked, e.target.checked]);
-  };
-
-  const handleChange2 = (e) => {
-    setChecked([e.target.checked, checked[1]]);
-  };
-
-  const handleChange3 = (e) => {
-    setChecked([checked[0], e.target.checked]);
-  };
 
   
   const navigate = useNavigate();
@@ -98,7 +140,7 @@ function SignUp() {
   let regex =         
   /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[$`~!@#$%^&\\(\\)\-_=+])(?!.*[^a-zA-Z0-9$`~!@#$%^&*#^?&\\(\\)\-_=+]).{8,20}$/;
 
-  return (
+return (
     <div className="SignUp">
       <div className="signUp">
         <h2>SIGN UP</h2>
@@ -149,82 +191,79 @@ function SignUp() {
               )}
             </div>
           </div>
-          <input type="text" placeholder="이름" value={name} onChange={handleName}/>
+          <input 
+            type="text" 
+            placeholder="이름" 
+            value={name} 
+            onChange={handleName}
+          />
           <br />
-          <input type="text" placeholder="휴대폰번호" value={phone} onChange={handlePhone} />
+          <div>
+            {
+              !nameValid && name.length > 0 &&(
+                <div>
+                  {
+                    <div>올바른 이름을 입력해주세요</div>
+                  }
+                </div>
+              )
+            }
+          </div>
+          <input 
+            type="text" 
+            placeholder="휴대폰번호" 
+            value={phone} 
+            onChange={handlePhone} 
+            />
           <br />
-          <button type="submit" disabled={notAllow}>
-            Sign Up
-          </button>
+            <div>
+            {
+              !phoneValid && phone.length > 0 && (
+                <div> ' - ' 을(를) 빼고 올바른 번호를 입력해주세요</div>
+              )
+            }
+            </div>
         </form>
+          <div>
+              <p>
+                  <a href='/terms'>Pluffy 이용약관</a>     <button onClick={onClickTermButton}>V</button>
+              </p>
+              <p>모두 확인하였으며 동의합니다
+                  <label>
 
+                      <input
+                          type="checkbox"
+                          checked={ParentChecked}
+                          value={checkbox}
+                          onChange={
+                          (e) =>  { setChecked(
+                            [e.target.checked, e.target.checked, e.target.checked, e.target.checked]
+                          );
+                          handleCheckbox(e)
+                          }
+                        }
+                          />
+                  </label>
+
+             
+                  <br></br>
+                  전체 동의에는 필수 정보에
+                  <br></br>대한 동의가 포함되어 있습니다
+              </p>
+              {terms && children}
+          </div>
         <div>
-          <p><a href='/terms'>Pluffy 이용약관</a></p>
-          <p>전체 동의에는 필수 정보에 <br></br>대한 동의가 포함되어 있습니다 
-          <button onClick={onClickTermButton}>V</button></p>
-
-          
-          {
-            terms === true ? 
-            (
-             <TerminateCheckbox />
-            ) 
-            : null
-          }
-        </div>
+    
+   
+     </div>
       </div>
+      <button><a href='/login'>logIn Page</a></button>
+      <button type="submit" disabled={notAllow}>Sign Up</button>
     </div>
   );
 }
 
 
-function TerminateCheckbox() {
-  const [checked, setChecked] = useState([false, false, false, false]);
-
-  const handleChange = (index, e) => {
-    const updatedChecked = [...checked];
-    updatedChecked[index] = e.target.checked;
-    setChecked(updatedChecked);
-  };
-
-  const ParentChecked = checked[0] && checked[1] && checked[2] && checked[3];
-
-  const children = (
-    <div style={{ display: 'flex', flexDirection: 'column', marginLeft: '20px' }}>
-      <label>
-        <input type="checkbox" checked={checked[0]} onChange={(e) => handleChange(0, e)} />
-        Child 1
-      </label>
-      <label>
-        <input type="checkbox" checked={checked[1]} onChange={(e) => handleChange(1, e)} />
-        Child 2
-      </label>
-      <label>
-        <input type="checkbox" checked={checked[2]} onChange={(e) => handleChange(2, e)} />
-        Child 3
-      </label>
-      <label>
-        <input type="checkbox" checked={checked[3]} onChange={(e) => handleChange(3, e)} />
-        Child 4
-      </label>
-    </div>
-  );
-
-  return (
-    <div>
-      <label>
-      <p> 모두 확인하였으며 동의합니다</p>
-        <input
-          type="checkbox"
-          checked={ParentChecked}
-          onChange={(e) => setChecked([e.target.checked, e.target.checked, e.target.checked, e.target.checked])}
-        />
-        Parent
-      </label>
-      {children}
-    </div>
-  );
-}
 
 
 export default SignUp;
