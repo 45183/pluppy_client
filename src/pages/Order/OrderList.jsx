d78d749f0d3487df74d20ac55579a76ad51c6fae
45183 <hsb9743@naver.com>
2024-02-23 15:00:08 +0900

    2/2

--- src/pages/Order/OrderList.jsx
+++ src/pages/Order/OrderList.jsx
@@ -1,7 +1,6 @@
 import Header from "../../components/Header/Header";
 import { useState, useEffect } from "react";
 import "./OrderList.css";
-import {Address_info} from '../MyPage/User_Modify';
 import axiosInstance from "../../utils/axios";
 
 const OrderList = (props) => {
@@ -12,16 +11,18 @@ const OrderList = (props) => {
   const [userData, setUserData] = useState(null); // 유저 정보 상태 추가
   const [productData, setProductData] = useState([]); // 상품 정보 상태 추가
 
-  let userId = ''; // 기본값으로 빈 문자열 설정
+  let userId = ""; // 기본값으로 빈 문자열 설정
 
-  const userIdCookie = document.cookie.split('; ').find(row => row.startsWith('userId'));
+  const userIdCookie = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("userId"));
   if (userIdCookie) {
-      userId = userIdCookie.split('=')[1];
+    userId = userIdCookie.split("=")[1];
   } else {
-      console.log('userId 쿠키가 없습니다.');
-  // 여기서 쿠키가 없는 경우에 대한 처리를 추가합니다.
-  // 예를 들어, 로그인 페이지로 리디렉션하거나 기본값으로 설정할 수 있습니다.
-  // userId = '기본값';
+    console.log("userId 쿠키가 없습니다.");
+    // 여기서 쿠키가 없는 경우에 대한 처리를 추가합니다.
+    // 예를 들어, 로그인 페이지로 리디렉션하거나 기본값으로 설정할 수 있습니다.
+    // userId = '기본값';
   }
 
   useEffect(() => {
@@ -30,9 +31,9 @@ const OrderList = (props) => {
       try {
         const res = await axiosInstance.get(`/user/${userId}`);
         setUserData(res.data); // 유저 정보 설정
-        console.log(res.data)
+        console.log(res.data);
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
     // cartItem으로 상품 정보 가져오기
@@ -41,30 +42,31 @@ const OrderList = (props) => {
         const res = await axiosInstance.get(`/cart/${userId}`);
         const cartItemsWithProductInfo = await Promise.all(
           res.data.data.map(async (cartItem) => {
-              // 각 장바구니 아이템의 상품 ID를 이용하여 해당 상품 정보를 가져옵니다.
-              const productRes = await axiosInstance.get(`/product/${cartItem.productId}`);
-              const productInfo = productRes.data.data;
-              console.log(productInfo)
-              // 상품 정보와 장바구니 아이템 정보를 합칩니다.
-              return {
-                  ...cartItem,
-                  productName: productInfo.name,
-                  productPrice: productInfo.price,
-                  productImage: productInfo.image1
-              };
+            // 각 장바구니 아이템의 상품 ID를 이용하여 해당 상품 정보를 가져옵니다.
+            const productRes = await axiosInstance.get(
+              `/product/${cartItem.productId}`
+            );
+            const productInfo = productRes.data.data;
+            console.log(productInfo);
+            // 상품 정보와 장바구니 아이템 정보를 합칩니다.
+            return {
+              ...cartItem,
+              productName: productInfo.name,
+              productPrice: productInfo.price,
+              productImage: productInfo.image1,
+            };
           })
-      );
+        );
         setProductData(cartItemsWithProductInfo); // 상품 정보 설정
-        console.log(cartItemsWithProductInfo)
+        console.log(cartItemsWithProductInfo);
       } catch (error) {
-        console.error('Error fetching product data:', error);
+        console.error("Error fetching product data:", error);
       }
     };
 
     fetchUserData(); // 유저 정보 가져오기 실행
     fetchProductData(); // 상품 정보 가져오기 실행
   }, [userId]); // userId가 변경될 때마다 실행
-
 
   const allBtnEvent = () => {
     if (allCheck === false) {
@@ -123,13 +125,24 @@ const OrderList = (props) => {
             <h4>기존 배송지</h4>
             {/* 유저 정보 표시 */}
             {userData && (
-            <>
-              <p>수령인 <input type="text" defaultValue={userData.data.name} /></p>
-              <p>주소 <input type="text" defaultValue={userData.data.address1} /></p>
-              <p>주소 <input type="text" defaultValue={userData.data.address2} /></p>
-              <p>연락처 <input type="text" defaultValue={userData.data.phone} /></p>
-            </>
-          )}
+              <div className="delivery_content">
+                <p>
+                  수령인 <input type="text" defaultValue={userData.data.name} />
+                </p>
+                <p>
+                  주소{" "}
+                  <input type="text" defaultValue={userData.data.address1} />
+                </p>
+                <p>
+                  주소{" "}
+                  <input type="text" defaultValue={userData.data.address2} />
+                </p>
+                <p>
+                  연락처{" "}
+                  <input type="text" defaultValue={userData.data.phone} />
+                </p>
+              </div>
+            )}
             <select>
               <option>배송시 요청사항을 선택해 주세요.</option>
               <option>문 앞에 놓아주시면 되요.</option>
@@ -139,18 +152,31 @@ const OrderList = (props) => {
           </div>
           <div className="product_info">
             <h2>상품정보</h2>
-            <table className="product_table">
-            <tbody>
+            <div className="product_zip">
               {productData.map((product) => (
-              <tr key={product.productId}>
-                <td><img src={`http://localhost:5000/${product.productImage}`} alt={productData.productName} /></td>
-                <td>{product.productName}</td>
-                <td>{product.amount}</td>
-                <td>{product.productPrice * product.amount}</td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
+                <div key={product.productId}>
+                  <img
+                    src={`http://localhost:8000/${product.productImage}`}
+                    alt={productData.productName}
+                  />
+                  <div className="productContent">
+                    <p>
+                      <h3>{product.productName}</h3>
+                    </p>
+                    <p>
+                      <strong>수량</strong> {product.amount}개
+                    </p>
+                    <p>
+                      <strong>가격</strong> {product.productPrice}원
+                    </p>
+                    <p>
+                      <strong>총 가격</strong>{" "}
+                      {product.productPrice * product.amount}원
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="payment">
             <h2>결제방법</h2>
@@ -164,9 +190,9 @@ const OrderList = (props) => {
         <div className="payment_price">
           <h2>결제금액</h2>
           <p>총 상품 금액</p>
-          <p id='discount'>할인금액</p>
+          <p id="discount">할인금액</p>
           <p>배송비</p>
-          <p id='all_price'>총 결제 금액</p>
+          <p id="all_price">총 결제 금액</p>
           <form method="post" action="">
             <div className="agree">
               <div>
@@ -182,41 +208,41 @@ const OrderList = (props) => {
                   </label>
                 </div>
                 <div>
-                  <input
-                    type="checkbox"
-                    id="check1"
-                    checked={useCheck}
-                    onChange={useBtnEvent}
-                  />
                   <label for="check1">
+                    <input
+                      type="checkbox"
+                      id="check1"
+                      checked={useCheck}
+                      onChange={useBtnEvent}
+                    />
                     <span>(필수)</span> 개인정보 수집 / 이용 동의
                   </label>
                 </div>
                 <div>
-                  <input
-                    type="checkbox"
-                    id="check2"
-                    checked={provideCheck}
-                    onChange={provideBtnEvent}
-                  />
                   <label for="check2">
+                    <input
+                      type="checkbox"
+                      id="check2"
+                      checked={provideCheck}
+                      onChange={provideBtnEvent}
+                    />
                     <span>(필수)</span> 개인정보 제3자 제공 동의
                   </label>
                 </div>
                 <div>
-                  <input
-                    type="checkbox"
-                    id="check3"
-                    checked={serviceCheck}
-                    onChange={serviceBtnEvent}
-                  />
                   <label for="check3">
+                    <input
+                      type="checkbox"
+                      id="check3"
+                      checked={serviceCheck}
+                      onChange={serviceBtnEvent}
+                    />
                     <span>(필수) </span>결제대행 서비스 이용약관 (주)KG이니시스
                   </label>
                 </div>
-                <p id='payment_p'>
-                  결제 및 계좌 안내 시 상호명은 (주)플러피로 표기되니
-                  참고 부탁드립니다.
+                <p id="payment_p">
+                  결제 및 계좌 안내 시 상호명은 (주)플러피로 표기되니 참고
+                  부탁드립니다.
                 </p>
               </div>
             </div>
@@ -225,7 +251,7 @@ const OrderList = (props) => {
             <a href="/payment_loading">
               <button>결제하기</button>
             </a>
-            </div>
+          </div>
         </div>
       </div>
     </div>
